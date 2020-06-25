3999a68a0094c540c58e249a4c6802ab66d65668
Gallegos-Batallanos-Carlos-Bryan <cgallegosb@unsa.edu.pe>
2020-06-25 17:16:16 -0500

    Template complete

--- ProyectoDestino/ProyectoFinal/src/Final/travello/views.py
+++ ProyectoDestino/ProyectoFinal/src/Final/travello/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+
+    dest1=Destination()
+    dest1.name="Mumbai"
+    dest1.img="destination_1.jpg"
+    dest1.desc="la ciudad es interesasnte"
+    dest1.price=700
+
+    dest2=Destination()
+    dest2.name="Destino 2"
+    dest2.img="destination_2.jpg"
+    dest2.desc="Este es el destino 2"
+    dest2.price="870"
+
+    dest3=Destination()
+    dest3.name="Destino 3"
+    dest3.img="destination_3.jpg"
+    dest3.desc="Este es el destino 3"
+    dest3.price="353"
+
+    dests=[dest1,dest2,dest3]
+
+    return render(request,"index.html", {'dests': dests})
