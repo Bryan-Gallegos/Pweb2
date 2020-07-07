6b7cab1873a361a036f17d87ce3294005cb247d9
Gallegos-Batallanos-Carlos-Bryan <cgallegosb@unsa.edu.pe>
2020-07-07 15:52:48 -0500

    mostrar nombre y descripcion de un Destino

--- ProyectoDestino/ProyectoFinal/src/Final/destinos/views.py
+++ ProyectoDestino/ProyectoFinal/src/Final/destinos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from travello.models import Destination
 from django.contrib.auth.models import User, auth
 # Create your views here.
 def add(request):
