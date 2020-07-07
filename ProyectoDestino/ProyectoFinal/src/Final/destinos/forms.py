72fd94c4c42ca0ec0d6575c3643deafaa82526be
Gallegos-Batallanos-Carlos-Bryan <cgallegosb@unsa.edu.pe>
2020-07-07 15:44:36 -0500

    Destinos app, para poder agregar, edit, borrar y ver destinos.

---
+++ ProyectoDestino/ProyectoFinal/src/Final/destinos/forms.py
@@ -0,0 +1,8 @@
+from django import forms 
+
+class RawDestinoForm(forms.Form):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
---
+++ ProyectoDestino/ProyectoFinal/src/Final/destinos/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+# Create your views here.
+def add(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        img=request.post['image']
+        desc=request.POST['desc']
+        price=request.POST['price']
+        offer=request.POST['offer']
+    else:
+        return render(request,'addDestiny.html')
+def remove(request):
+    return render(request,'removeDestiny.html')
+def edit(request):
+    return render(request,'editDestiny.html')
+def show(request,myID):
+    obj=Destination.objects.get(id=myID)
+    context ={
+        'objeto':obj,
+    }
+    return render(request,'showDestiny.html',context)
