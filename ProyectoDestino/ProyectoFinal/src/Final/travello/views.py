a3dc5594babbded8c84e9ceba09849fab2951b29
Gallegos-Batallanos-Carlos-Bryan <cgallegosb@unsa.edu.pe>
2020-07-09 15:02:04 -0500

    solucionando errores en los views

--- ProyectoDestino/ProyectoFinal/src/Final/travello/views.py
+++ ProyectoDestino/ProyectoFinal/src/Final/travello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .models import Destination
 from django.contrib.auth.models import User, auth
-from .forms import PersonForm,RawDestinoForm
+from .forms import DestinoForm,RawDestinoForm
 # Create your views here.
 def index(request):
 
@@ -11,13 +11,24 @@ def index(request):
     return render(request,"index.html", {'dests': dests})
 
 def add(request):
-    form = RawPersonaForm()
+    form = RawDestinoForm() #request.GET
+    if request.method == "POST":
+        form = RawDestinoForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            Destination.objects.create(**form.cleanned_data)
+        else:
+            print(form.errors)
+    context={
+        'form' :form,
+    }
+    return render(request,'addDestiny.html',context)
 def remove(request,myID):
     obj=get_object_or_404(Destination,id=myID)
     if request.method=='POST':
         messages.info(request,'Remove Destiny')
         obj.delete()
-        return render('../')
+        return redirect('../')
     context ={
         'objeto':obj,
     }
