7627c505db31ed03fa93fce9b1c9d59f6a4f8435
Gallegos-Batallanos-Carlos-Bryan <cgallegosb@unsa.edu.pe>
2020-07-16 18:08:07 -0500

    cambios en el model MedicalCite

--- ProyectoEmpresa/CitasMedicas/src/CitasMedicas/cita/models.py
+++ ProyectoEmpresa/CitasMedicas/src/CitasMedicas/cita/models.py
@@ -3,6 +3,8 @@ from datetime import datetime
 # Create your models here.
 class MedicalCite(models.Model):
     name=models.CharField(max_length=100)
+    doctor=models.CharField(max_length=40)
     area=models.TextField()
+    hora=models.TimeField()
     fecha=models.DateField()
     
