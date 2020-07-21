5634acacd7c5308822e4ce997c8712b9f6d6afc4
Gallegos-Batallanos-Carlos-Bryan <cgallegosb@unsa.edu.pe>
2020-07-21 14:12:41 -0500

    model Doctor creado

--- ProyectoEmpresa/CitasMedicas/src/CitasMedicas/cita/models.py
+++ ProyectoEmpresa/CitasMedicas/src/CitasMedicas/cita/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from datetime import datetime
 # Create your models here.
 class Cite(models.Model):
-    name=models.CharField(max_length=100)
-    doctor=models.CharField(max_length=40)
+    name=models.CharField(max_length = 100)
+    doctor=models.CharField(max_length = 40)
     area=models.TextField()
     hora=models.TimeField()
     fecha=models.DateField()
     
+class Doctor(models.Model):
+    name=models.CharField(max_length = 100)
+    last_name=models.CharField(max_length = 150)
+    specialty=models.CharField(max_length = 50)
+    code=models.IntegerField(max_length=5)
