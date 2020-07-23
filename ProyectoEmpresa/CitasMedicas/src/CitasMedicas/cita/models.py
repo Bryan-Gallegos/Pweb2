d54a4ce237b401ee714363ce5e243a9ca96730a9
Gallegos-Batallanos-Carlos-Bryan <cgallegosb@unsa.edu.pe>
2020-07-23 14:15:07 -0500

    cambios en model doctor

--- ProyectoEmpresa/CitasMedicas/src/CitasMedicas/cita/models.py
+++ ProyectoEmpresa/CitasMedicas/src/CitasMedicas/cita/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 # Create your models here.
 class Doctor(models.Model):
     name=models.CharField(max_length = 100)
+    img=models.ImageField(upload_to='pics')
     last_name=models.CharField(max_length = 150)
     specialty=models.CharField(max_length = 50)
     code=models.IntegerField()
